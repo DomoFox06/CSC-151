a20a4f7d7bc64e13c60b69387b449ab75b1d1e85
DomoFox <yarnalld0401@student.faytechcc.edu>
2026-01-31 06:22:13 +0000

    save

---
+++ GroupProjectProgramIdeas.java
@@ -0,0 +1,13 @@
+/*Domonic Yarnall
+Group Project */
+
+public class GroupProjectProgramIdeas {
+
+
+    public static void main(String[] args){
+        System.out.println("Football Team:");
+    }
+
+
+
+}
